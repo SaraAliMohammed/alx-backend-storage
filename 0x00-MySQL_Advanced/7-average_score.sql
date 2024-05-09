3422cb9e87d85943345c3c722349dd66728ab880
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-05-09 11:20:40 +0000

    Task7. Average score

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,16 @@
+-- Task7. Average score - creates a stored procedure ComputeAverageScoreForUser.
+-- Computes and store the average score for a student.
+-- Note: An average score can be a decimal.
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+BEGIN
+	UPDATE users
+	SET
+	average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE id = user_id;
+
+END $$
+
+DELIMITER ;
